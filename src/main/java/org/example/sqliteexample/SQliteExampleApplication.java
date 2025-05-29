2e55c21232ebcd0b9d957fd6376f1be5595eb471
vngocvan <vngocvan@gmail.com>
2025-05-29 22:09:04 +0700

    init

---
+++ src/main/java/org/example/sqliteexample/SQliteExampleApplication.java
@@ -0,0 +1,13 @@
+package org.example.sqliteexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SQliteExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SQliteExampleApplication.class, args);
+    }
+
+}
